8bcb39a2cdae081dbfcb02ca3a84702a77659f58
alex <ong.a998@gmail.com>
2024-04-08 19:21:27 +0800

    compensation

--- resources/views/hr/time-keeping.blade.php
+++ resources/views/hr/time-keeping.blade.php
@@ -24,13 +24,15 @@
         </div>
 
         <!-- Add record btn -->
-        <div class="flex justify-end">
+        <div class="flex justify-end h-8">
+            @can('for-compensation')
             <a href="{{ route('add-dtr') }}" class="inline-flex items-center px-3 py-2 border-0 text-sm leading-4 font-medium rounded-md text-gray-500">
                 Add Record
                 <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                 </svg>
             </a>
+            @endif
         </div>
         
         @if(session('message'))
@@ -52,7 +54,6 @@
                     <th class="w-20 text-center border border-black">Undertime (hrs)</th>
                     <th class="w-20 text-center border border-black">Overtime (hrs)</th>
                     <th class="w-20 text-center border border-black">Late time (hrs)</th>
-                    <th class="w-10 text-center border border-black">LC</th>
                     <th class="w-10"></th>
                 </tr>
             </thead>
@@ -65,8 +66,7 @@
                     <td class="w-20 text-center border border-black"></td>
                     <td class="w-28 text-center border border-black"></td>
                     <td class="w-28 text-center border border-black"></td>
-                    <td class="w-9 pl-2.5 border border-black"></td>
-                    <td class="w-9 pl-2.5">
+                    <td class="w-9 pl-3">
                         <button type="text" class="w-10 h-6 bg-purple-600 rounded flex justify-center items-center">
                         <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M16 18C16 16.6667 15.7778 15.5 15.5556 15.3333C15.3333 15.1667 13.7778 15 12 
@@ -92,7 +92,6 @@
                     <td class="w-28 text-center border-r border-black">{{$data->overtime}}</td>
                     <td class="w-28 text-center border-r border-black">{{$data->late}}</td>
                     <td class="w-9 pl-2.5 border-r border-black"></td>
-                    <td class="w-9 pl-2.5"></td>
                 </tr>
                 @endforeach
             </tbody>
